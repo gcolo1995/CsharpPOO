4fe8d85ef11a7b76abda456937215f40437217e6
Gabriel Colombano <101367584+gcolo1995@users.noreply.github.com>
2023-04-11 20:59:33 -0300

    Add files via upload

---
+++ Propio_Auto/Propio_Auto/Auto.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Propio_Auto
+{
+    internal class Auto
+    {
+        private string _marca;
+        private string _modelo;
+        private int _velocidad;
+        private int _ano;
+
+        public string Marca
+        {                                         
+            get { return _marca; }
+            set { _marca = value; }
+        }
+        public int Ano
+        {
+            get { return _ano; }
+            set { _ano = value; }
+        }
+        public int Velocidad
+        {
+            get { return _velocidad; }
+            set { _velocidad = value; }
+        }
+        public string Modelo
+        {
+            get { return _modelo; }
+            set { _modelo = value; }
+        }
+
+
+        public void Acelerar()
+        {
+            _velocidad++;
+        }
+        public void Acelerar(int velocidad)
+        {
+            _velocidad += velocidad;
+        }
+        public void Desacelerar()
+        {
+            if (_velocidad >= 0) _velocidad--;
+        }
+
+
+
+    }
+}
---
+++ Propio_Auto/Propio_Auto/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Propio_Auto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Auto miAuto1;                           //estoy declarando un objeto (en null, no tiene los atributos ni los metodos del objeto)
+            Auto miAuto1 = new Auto();
+            miAuto1.Acelerar();
+
+
+        }
+
+    }
+}
