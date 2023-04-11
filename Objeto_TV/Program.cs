a19ee2a0bcd7e7f235a12751a8722ba60402fcdd
Gabriel Colombano <101367584+gcolo1995@users.noreply.github.com>
2023-04-11 08:31:00 -0300

    Add files via upload

---
+++ Objeto_TV/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objeto_TV
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Televisor miTelevisor = new Televisor();
+            miTelevisor.SubirVolumen();
+
+        }
+    }
+}
---
+++ Objeto_TV/Televisor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objeto_TV
+{
+    internal class Televisor
+    {
+    }
+}
