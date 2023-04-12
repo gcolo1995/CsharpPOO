d65e5429c4978c09d57f85c81cd07831c50219d6
Gabriel Colombano <101367584+gcolo1995@users.noreply.github.com>
2023-04-11 21:20:10 -0300

    Add files via upload

---
+++ Caja_de_Ahorro/Caja_de_Ahorro/CajaAhorro.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caja_de_Ahorro
+{
+    public class CajaAhorro
+    {
+        #region ATRIBUTOS
+        private int _numero;
+        private string _tipoMoneda;
+        private int _saldo = 0;
+        private string _titular;
+        #endregion 
+
+        #region PROPIEDADES
+        public int Numero
+        {                
+            get { return _numero; }
+            set { _numero = value; }
+        }
+
+        public string TipoMoneda
+        {
+            get { return _tipoMoneda; }
+            set { _tipoMoneda = value; }
+        }
+
+        public int Saldo
+        {
+            get { return _saldo; }
+        }
+
+        public string Titular
+        {
+            get { return _titular; }
+            set { _titular = value; }
+        }
+        #endregion
+
+        #region METODOS
+        public void Depositar(int monto)
+        {
+            _saldo += monto;
+        }
+
+        public string Extraer(int monto)
+        {
+            if (_saldo >= monto)
+            {
+                _saldo -= monto;
+                return "Se han extraido $ "+monto+". El nuevo saldo de su cuenta es de $ "+_saldo+"."; 
+            }
+            else
+                return "No dispone de fondos suficientes";
+        }
+        #endregion
+    }
+}
---
+++ Caja_de_Ahorro/Caja_de_Ahorro/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caja_de_Ahorro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CajaAhorro miCajaAhorro = new CajaAhorro();
+
+            Console.WriteLine("Ingrese el monto a depositar");            
+            int deposito = int.Parse(Console.ReadLine());
+            miCajaAhorro.Depositar(deposito);
+
+            Console.WriteLine("Ingrese el monto a extraer");
+            int extraccion = int.Parse(Console.ReadLine());           
+            Console.WriteLine(miCajaAhorro.Extraer(extraccion));
+
+            Console.ReadKey();
+        }
+
+    }
+}
