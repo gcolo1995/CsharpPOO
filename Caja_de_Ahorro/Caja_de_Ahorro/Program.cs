408ab032cfaf3b423c2a082ceda5128d036bac01
Gabriel Colombano <101367584+gcolo1995@users.noreply.github.com>
2023-04-11 22:41:29 -0300

    Update Program.cs

--- Caja_de_Ahorro/Caja_de_Ahorro/Program.cs
+++ Caja_de_Ahorro/Caja_de_Ahorro/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,18 +10,53 @@ namespace Caja_de_Ahorro
     {
         static void Main(string[] args)
         {
+
+            /*Console.WriteLine("¿Desea dar de alta una cuenta?");
+            if(Console.ReadLine().ToUpper() == "SI") 
+            {
+                CajaAhorro miCajaAhorro = new CajaAhorro();            
+            }*/
+            
             CajaAhorro miCajaAhorro = new CajaAhorro();
 
-            Console.WriteLine("Ingrese el monto a depositar");            
-            int deposito = int.Parse(Console.ReadLine());
-            miCajaAhorro.Depositar(deposito);
+            Console.WriteLine("Ingrese el titular de la cuenta");
+            miCajaAhorro.Titular = Console.ReadLine();
 
-            Console.WriteLine("Ingrese el monto a extraer");
-            int extraccion = int.Parse(Console.ReadLine());           
-            Console.WriteLine(miCajaAhorro.Extraer(extraccion));
+            Console.WriteLine("Ingrese el numero de la cuenta");
+            miCajaAhorro.Numero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la moneda de la cuenta");
+            miCajaAhorro.TipoMoneda = Console.ReadLine().ToUpper();
+
+            int opcion = 1;
+            do
+            {
+                Console.WriteLine("Ingrese el número de la operación que desea realizar" +
+                "\n \t 1 - Consultar Saldo \n \t 2 - Depositar \n \t 3 - Extraer dinero \n \t 9 - Salir");
+                opcion = int.Parse(Console.ReadLine());
+            }while (opcion == 1 && opcion == 2 && opcion == 3 && opcion == 9);
+            switch (opcion)
+            {
+                case 1:
+                    Console.WriteLine("El saldo de su caja de ahorro es $"+miCajaAhorro.Saldo+".");
+                    break;
+                case 2:
+                    Console.WriteLine("Ingrese el monto a depositar");
+                    int deposito = int.Parse(Console.ReadLine());
+                    miCajaAhorro.Depositar(deposito);
+                    break;
+                case 3:
+                    Console.WriteLine("Ingrese el monto a extraer");
+                    int extraccion = int.Parse(Console.ReadLine());
+                    Console.WriteLine(miCajaAhorro.Extraer(extraccion));
+                    break;
+                case 9:
+                    break;
+
+            }
+            Console.WriteLine("Gracias por operar con nosotros.");
 
             Console.ReadKey();
         }
-
     }
 }
