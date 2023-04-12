b44f46b5ce227bc802ce993545ac97f9d4c7c623
Gabriel Colombano <101367584+gcolo1995@users.noreply.github.com>
2023-04-11 22:53:12 -0300

    Update Program.cs

--- Caja_de_Ahorro/Caja_de_Ahorro/Program.cs
+++ Caja_de_Ahorro/Caja_de_Ahorro/Program.cs
@@ -29,30 +29,37 @@ namespace Caja_de_Ahorro
             miCajaAhorro.TipoMoneda = Console.ReadLine().ToUpper();
 
             int opcion = 1;
-            do
+            while(opcion != 9)
             {
-                Console.WriteLine("Ingrese el número de la operación que desea realizar" +
-                "\n \t 1 - Consultar Saldo \n \t 2 - Depositar \n \t 3 - Extraer dinero \n \t 9 - Salir");
-                opcion = int.Parse(Console.ReadLine());
-            }while (opcion == 1 && opcion == 2 && opcion == 3 && opcion == 9);
-            switch (opcion)
-            {
-                case 1:
-                    Console.WriteLine("El saldo de su caja de ahorro es $"+miCajaAhorro.Saldo+".");
-                    break;
-                case 2:
-                    Console.WriteLine("Ingrese el monto a depositar");
-                    int deposito = int.Parse(Console.ReadLine());
-                    miCajaAhorro.Depositar(deposito);
-                    break;
-                case 3:
-                    Console.WriteLine("Ingrese el monto a extraer");
-                    int extraccion = int.Parse(Console.ReadLine());
-                    Console.WriteLine(miCajaAhorro.Extraer(extraccion));
-                    break;
-                case 9:
-                    break;
-
+                do
+                {
+                    Console.WriteLine("Ingrese el número de la operación que desea realizar" +
+                    "\n \t 1 - Consultar Saldo \n \t 2 - Depositar \n \t 3 - Extraer dinero \n \t 4 -" +
+                    " Consultar datos de la cuenta \n \t 9 - Salir");
+                    opcion = int.Parse(Console.ReadLine());
+                } while (opcion == 1 && opcion == 2 && opcion == 3 && opcion == 9 && opcion == 4);
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine("El saldo de su caja de ahorro es $" + miCajaAhorro.Saldo + ".");
+                        break;
+                    case 2:
+                        Console.WriteLine("Ingrese el monto a depositar");
+                        int deposito = int.Parse(Console.ReadLine());
+                        miCajaAhorro.Depositar(deposito);
+                        break;
+                    case 3:
+                        Console.WriteLine("Ingrese el monto a extraer");
+                        int extraccion = int.Parse(Console.ReadLine());
+                        Console.WriteLine(miCajaAhorro.Extraer(extraccion));
+                        break;
+                    case 4:
+                        Console.WriteLine("Los datos de su cuenta son: \n \tTitular: " + miCajaAhorro.Titular + "" +
+                            "\n \tNúmero de Cuenta: " + miCajaAhorro.Numero + " \n \tTipo: CA " + miCajaAhorro.TipoMoneda + "");
+                        break;
+                    case 9:
+                        break;
+                }
             }
             Console.WriteLine("Gracias por operar con nosotros.");
 
