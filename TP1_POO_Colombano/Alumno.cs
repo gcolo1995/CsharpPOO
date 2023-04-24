ffd4e661ed2c19bb56bcd027eca7052dec8cd6bc
Gabriel Colombano <101367584+gcolo1995@users.noreply.github.com>
2023-04-24 02:34:35 -0300

    Add files via upload
    Consigna del programa:
    
    Nos solicitan crear un programa que maneje una lista de alumnos.
    Un alumno posee:
    
    Características
    Legajo int lectura/escritura 
    Nombre string lectura/escritura 
    Apellido string lectura/escritura 
    Fecha_Nacimiento date solo escritura 
    Fecha_Ingreso date solo escritura 
    Edad int solo lectura 
    Activo boolean lectura escritura 
    Cant_Materia_Aprobadas int solo escritura 
    
    Métodos
    Antigüedad int Retorna la cantidad de años/meses/días que hace que el alumno asiste a la institución. El método posee un parámetro que indica en que unidad(años, meses, días) se desea el retorno. Para el cálculo se considera solo año/mes cumplido, que hace que el alumno acude a la institución. Para determinar el número de años se considera la fecha actual y la fecha de ingreso. 
    Materias_No_Aprobadas int Retorna la cantidad de materias no aprobadas a la fecha sabiendo que la carrera posee 36 Materias y considerando la cantidad de materias aprobadas.
    Edad_De_Ingreso int Retorna la edad a la que el alumno ingresó a la institución considerando la fecha de nacimiento y la fecha de ingreso.
    
    Constructores:
    1. Constructor sin parámetros
    2. Constructor con todos los parámetros que permiten inicializar las propiedades.
    
    Finalizadores:
    1. Que cuando el objeto queda liberado muestre una leyenda indicando el Legajo, Nombre y Apellido del Alumno.
    
    Nos solicitan que la GUI (interfaz gráfica del usuario) permita visualizar la lista de los alum-nos ingresados en una lista del tipo DataGridView.
    
    La GUI debe tener botones para:
    Agregar un alumno a la lista.
    Borrar el alumno seleccionado de la lista.
    Modificar el alumno seleccionado de la lista.
    
    La GUI debe tener cajas de texto para ver del alumno seleccionado en la grilla. Estas cajas de texto se deben actualizar cuando el usuario de desplaza por las filas de la grilla con el cursor o les hace clic
    Antigüedad
    Materias_No_Aprobadas
    Edad_De_Ingreso

---
+++ TP1_POO_Colombano/Alumno.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1_POO_Colombano
+{
+    public class Alumno
+    {
+        #region atributes
+        private int _legajo;
+        private string _nombre;
+        private string _apellido;
+        private DateTime _fecha_Nacimiento;
+        private DateTime _fecha_Ingreso;
+        private int _edad;
+        private Boolean _activo;
+        private int _cant_Materias_Aprobadas;
+        #endregion
+
+        #region properties
+        public int Legajo
+        {
+            get { return _legajo; }
+            set { _legajo = value; }
+        }
+        public string Nombre
+        {
+            get { return _nombre;}
+            set { _nombre = value; }
+        }
+        public string Apellido
+        {
+            get { return _apellido; }
+            set { _apellido = value; }
+        }
+        public DateTime FechaNacimiento
+        {
+            set { _fecha_Nacimiento = value; }
+        }
+        public DateTime FechaIngreso
+        {
+            set { _fecha_Ingreso = value; }
+        }
+        public int Edad
+        {
+            get
+            {
+                TimeSpan _tiempo = DateTime.Now.Subtract(_fecha_Nacimiento);
+                _edad = Convert.ToInt32(Math.Truncate(_tiempo.TotalDays / 365.25));
+                return _edad;
+
+            }
+        }
+        public Boolean Activo
+        {
+            get { return _activo;}
+            set { _activo = value; }
+        }
+        public int CantAprobadas
+        {
+            set { _cant_Materias_Aprobadas = value; }
+        }
+        #endregion
+
+        #region metods
+
+        public int Antiguedad(string unidad)            //la unidad ya va a estar convertida en .Year
+        {
+            int _tiempo = 0;
+            
+            if (unidad == "Años")
+            {
+                _tiempo = DateTime.Now.Year - _fecha_Ingreso.Year;
+                if( DateTime.Now.DayOfYear < _fecha_Ingreso.DayOfYear )
+                {
+                    _tiempo--;
+                }
+            }
+            else if (unidad == "Meses")
+            {
+                int _ano = DateTime.Now.Year - _fecha_Ingreso.Year;
+                int _meses = DateTime.Now.Month - _fecha_Ingreso.Month;
+                if (DateTime.Now.DayOfYear < _fecha_Ingreso.DayOfYear)
+                {
+                    _meses--;
+                }
+
+                _tiempo = _ano * 12 + _meses ;
+            }
+            else if (unidad == "Dias")    
+            {
+                TimeSpan _dias = DateTime.Now.Subtract(_fecha_Ingreso);
+                _tiempo = Convert.ToInt32(_dias.TotalDays);
+            }
+
+            return _tiempo;
+
+        }
+
+        public int Materias_No_Aprobadas()
+        {
+            int _cantMaterias = 36;
+            return (_cantMaterias - _cant_Materias_Aprobadas);
+        }
+
+        public int Edad_De_Ingreso()
+        {
+            TimeSpan _tiempo = _fecha_Ingreso - _fecha_Nacimiento;
+            return Convert.ToInt32(Math.Truncate(_tiempo.TotalDays / 365.25));
+        }
+
+        #endregion
+
+    }
+}
