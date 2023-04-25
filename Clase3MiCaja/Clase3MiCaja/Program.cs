44e2d37d025190bbbd6f721ea57a8ab904ab6ec0
Gabriel Colombano <101367584+gcolo1995@users.noreply.github.com>
2023-04-25 20:25:53 -0300

    Update Program.cs

--- Clase3MiCaja/Clase3MiCaja/Program.cs
+++ Clase3MiCaja/Clase3MiCaja/Program.cs
@@ -10,8 +10,8 @@ namespace Clase3MiCaja
     {
         static void Main(string[] args)
         {
-            CajaAhorro PrimerCA = new CajaAhorro();
-            Titular PrimerTitular = new Titular();
+            CajaAhorro PrimerCA = new CajaAhorro();         //podría escribirse " var PrimerCA = new CajaAhorro(); "
+            Titular PrimerTitular = new Titular();          //var PrimerTitular = new Titular();
 
             Console.WriteLine("Ingrese el Nombre: ");
             PrimerTitular.Nombre = Console.ReadLine();
@@ -24,15 +24,16 @@ namespace Clase3MiCaja
 
             Console.WriteLine("Ingrese el sexo: (Masculino/Femenino)");
             var genero = Console.ReadLine();
-            //Console.WriteLine("Dato guardado en genero: "+genero+"");
+            Console.WriteLine("Dato guardado en genero: "+genero+"");
 
             if (Enum.TryParse(genero, out Genero sexo))
             {
-                //Console.WriteLine("Dato guardado en genero: " + sexo + "");
+                Console.WriteLine("Dato guardado en sexo: " + sexo + "");
                 PrimerTitular.Sexo = sexo; 
-            }    
+            }
+            Console.WriteLine("Dato guardado en sexo: " + sexo + "");
 
-            
+
             Console.WriteLine("Ingrese la fecha de nacimiento (DD/MM/AAAA)");
             PrimerTitular.FechaNac = Convert.ToDateTime(Console.ReadLine());
             Console.WriteLine("La edad del cliente es de "+ PrimerTitular.Edad +" años.");
