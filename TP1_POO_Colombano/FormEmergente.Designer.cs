ad3dc5e7254006a23e97366dfb9b3d28e742d953
Gabriel Colombano <101367584+gcolo1995@users.noreply.github.com>
2023-05-08 07:15:40 -0300

    Add files via upload

---
+++ TP1_POO_Colombano/FormEmergente.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace TP1_POO_Colombano
+{
+    partial class FormEmergente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.dateNacimiento = new System.Windows.Forms.DateTimePicker();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.dateIngreso = new System.Windows.Forms.DateTimePicker();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblLegajo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Apellido";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 26);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Fecha de \r\nNacimiento";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 26);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Fecha de \r\nIngreso";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 168);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 26);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Materias\r\nAprobadas";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(81, 38);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(125, 20);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(81, 62);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(125, 20);
+            this.txtApellido.TabIndex = 6;
+            // 
+            // dateNacimiento
+            // 
+            this.dateNacimiento.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateNacimiento.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.dateNacimiento.Location = new System.Drawing.Point(81, 92);
+            this.dateNacimiento.MaxDate = new System.DateTime(2023, 5, 1, 0, 0, 0, 0);
+            this.dateNacimiento.MinDate = new System.DateTime(1900, 1, 1, 0, 0, 0, 0);
+            this.dateNacimiento.Name = "dateNacimiento";
+            this.dateNacimiento.Size = new System.Drawing.Size(125, 20);
+            this.dateNacimiento.TabIndex = 7;
+            this.dateNacimiento.Value = new System.DateTime(1960, 1, 1, 0, 0, 0, 0);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(86, 167);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(119, 20);
+            this.numericUpDown1.TabIndex = 9;
+            // 
+            // dateIngreso
+            // 
+            this.dateIngreso.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateIngreso.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.dateIngreso.Location = new System.Drawing.Point(81, 128);
+            this.dateIngreso.MaxDate = new System.DateTime(2023, 5, 1, 0, 0, 0, 0);
+            this.dateIngreso.MinDate = new System.DateTime(1900, 1, 1, 0, 0, 0, 0);
+            this.dateIngreso.Name = "dateIngreso";
+            this.dateIngreso.Size = new System.Drawing.Size(125, 20);
+            this.dateIngreso.TabIndex = 10;
+            this.dateIngreso.Value = new System.DateTime(2015, 1, 1, 0, 0, 0, 0);
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(16, 201);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(86, 23);
+            this.btnAceptar.TabIndex = 11;
+            this.btnAceptar.Text = "ACEPTAR";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(118, 201);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(87, 23);
+            this.btnCancelar.TabIndex = 12;
+            this.btnCancelar.Text = "CANCELAR";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Legajo";
+            // 
+            // lblLegajo
+            // 
+            this.lblLegajo.AutoSize = true;
+            this.lblLegajo.Location = new System.Drawing.Point(78, 9);
+            this.lblLegajo.Name = "lblLegajo";
+            this.lblLegajo.Size = new System.Drawing.Size(0, 13);
+            this.lblLegajo.TabIndex = 14;
+            // 
+            // FormEmergente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(238, 236);
+            this.Controls.Add(this.lblLegajo);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.dateIngreso);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.dateNacimiento);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormEmergente";
+            this.Text = "FormEmergente";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.DateTimePicker dateNacimiento;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.DateTimePicker dateIngreso;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblLegajo;
+    }
+}
---
+++ TP1_POO_Colombano/FormEmergente.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP1_POO_Colombano
+{
+    public partial class FormEmergente : Form
+    {
+        //private static int ContadorLegajo = 1;
+        public ListadoAlumno listaEmerg;
+
+        public FormEmergente()
+        {
+            
+            InitializeComponent();
+            
+            lblLegajo.Text = "Pendiente";
+            numericUpDown1.Maximum = 36;     //el enunciado indica que son 36 materias
+            dateNacimiento.MaxDate = DateTime.Today;
+            dateIngreso.MaxDate = DateTime.Today;
+            dateIngreso.MinDate = dateNacimiento.Value;
+
+        }
+        public FormEmergente(Alumno alumno)
+        {
+            InitializeComponent();
+            
+            lblLegajo.Text = alumno.Legajo.ToString();
+            txtNombre.Text = alumno.Nombre;
+            txtApellido.Text = alumno.Apellido;
+            dateNacimiento.Value = alumno.FechaNacimiento;
+            dateIngreso.Value = alumno.FechaIngreso;
+            numericUpDown1.Value = (36 - alumno.CalcularMateriasNoAprob());
+
+            numericUpDown1.Maximum = 36;        //el enunciado indica que son 36 materias
+            dateNacimiento.MaxDate = DateTime.Today;
+            dateIngreso.MaxDate = DateTime.Today;
+            dateIngreso.MinDate = dateNacimiento.Value;
+
+        }
+
+        
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            var alumno = new Alumno();
+
+            Principal formPrincipal = (Principal)this.Owner;
+            
+            if (formPrincipal.lista.BuscarNombreApellidoNac(txtNombre.Text, txtApellido.Text, dateNacimiento.Value) != null)
+            {
+                MessageBox.Show("El alumno ya se encontraba cargado \n" +
+                        "con el legajo nº " + formPrincipal.lista.BuscarNombreApellidoNac(txtNombre.Text, txtApellido.Text, dateNacimiento.Value) + ".");
+
+
+                
+            }
+            else
+            {
+                alumno.Nombre = txtNombre.Text;
+                alumno.Apellido = txtApellido.Text;
+                alumno.FechaNacimiento = dateNacimiento.Value;
+                alumno.FechaIngreso = dateIngreso.Value;
+                alumno.CantAprobadas = (int)numericUpDown1.Value;
+                //alumno.Legajo = ContadorLegajo;
+                formPrincipal.lista.AgregarAlumno(alumno);
+
+                MessageBox.Show("Alumno cargado.\n Número de legajo " + alumno.Legajo + ".");
+                //ContadorLegajo++;
+                this.DialogResult = DialogResult.OK;
+            }
+        this.Close();
+            
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Acción cancelada.");
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+    }
+}
