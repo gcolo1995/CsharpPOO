52a0a89320b402868d35dd17f3c56d68aaf2f46f
Gabriel Colombano <101367584+gcolo1995@users.noreply.github.com>
2023-05-09 23:02:03 -0300

    Add files via upload

---
+++ Clase6_Ejemplo_Pasajero/Clase6_Ejemplo_Pasajero/Colectivo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase6_Ejemplo_Pasajero
+{
+    internal class Colectivo
+    {
+        List<Pasajero> pasajeros;
+
+        public Colectivo()
+        {
+            pasajeros = new List<Pasajero>();
+
+                        
+        }
+
+        public void VenderBoleto(Pasajero pasajero)
+        {
+            pasajeros.Add(pasajero);
+        }
+        /*public void MostrarListado()
+        {
+            MessageBox.Show(" " + Pasajero.Nombre + "");
+        }*/
+    }
+}
---
+++ Clase6_Ejemplo_Pasajero/Clase6_Ejemplo_Pasajero/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase6_Ejemplo_Pasajero
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Pasajero pasajero = new Pasajero();
+            Estudiante estudiante = new Estudiante();
+                        
+            
+            Colectivo colectivo = new Colectivo();
+            estudiante.Nombre = "Gabriel";
+            estudiante.Apellido = "Batistuta";
+            estudiante.Edad = 25;
+            estudiante.Institucion = "UAI";
+            estudiante.NroLegajo = 1;
+
+            colectivo.VenderBoleto(estudiante);
+
+            colectivo.VenderBoleto(estudiante);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
